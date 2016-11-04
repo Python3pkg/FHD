23a15493da24c502e9ccc3a354861a09fc0fcb0f
Ruby Byrne <rubylinderbyrne@gmail.com>
2016-11-03 21:23:39 -0400

    changed aws_firstpass_wrapper.py structure

--- Observations/aws_firstpass_wrapper.py
+++ Observations/aws_firstpass_wrapper.py
@@ -19,14 +19,42 @@ def main():
 	cotter_subversion = 1
 		
 	try:	
-		download_obsids(obs_id, cotter_version, cotter_subversion)
+		#Retrieve uvfits and metafits files from S3
+		if not os.path.isdir('/tmp/uvfits'):
+			os.system('mkdir /tmp/uvfits')
+		directory_contents = os.listdir('/tmp/uvfits')
+
+		download_uvfits = True
+		download_metafits = True
+		for filename in directory_contents:
+			if filename == '{}.uvfits'.format(obs_id):
+				download_uvfits = False
+			if filename == '{}.metafits'.format(obs_id):
+				download_metafits = False
+		if download_uvfits:
+			print 'Downloading {}.uvfits from S3...'.format(obs_id)
+			sys.stdout.flush()
+			os.system('aws s3 cp s3://mwatest/uvfits/{}.{}/{}.uvfits /tmp/uvfits/{}.uvfits'.format(cotter_version, cotter_subversion, obs_id, obs_id))
+		if download_metafits:
+			print 'Downloading {}.metafits from S3...'.format(obs_id)
+			sys.stdout.flush()
+			os.system('aws s3 cp s3://mwatest/metafits/{}.{}/{}.metafits /tmp/uvfits/{}.metafits'.format(cotter_version, cotter_subversion, obs_id, obs_id))
 	except:
 		print 'ERROR downloading obsid {} from S3.'.format(obs_id)
 		sys.stdout.flush()
 		error_procedure(log_filename)
 	
 	try:
-		run_firstpass(obsids, version)
+		#Run firstpass
+		vis_file_list = '/tmp/uvfits/{}.uvfits'.format(obs_id)
+		output_directory = '/tmp'
+		if not os.path.isdir('/tmp/{}'.format(version)):
+			os.system('mkdir /tmp/{}'.format(version))
+		idl = pidly.IDL('/usr/local/bin/idl -IDL_DEVICE ps')
+		print 'Running firstpass on obsid {}...'.format(obs_id)
+		sys.stdout.flush()
+		idl('eor_firstpass_versions, /aws, obs_id = \'{}\', output_directory = \'{}\', version = \'{}\', vis_file_list = \'{}\''.format(obs_id, output_directory, version, vis_file_list))
+		idl.close()
 	except:
 		print 'ERROR running firstpass.'
 		sys.stdout.flush()
@@ -49,47 +77,10 @@ def main():
 	
 	#Copy log file to S3
 	os.system('aws s3 cp /tmp/{} s3://mwatest/FHD_FIRST_PASS/{}'.format(log_filename, log_filename))
-	
-	
-def download_obsids(obs_id, cotter_version, cotter_subversion):
 
-	#Retrieve uvfits and metafits files from S3
-	if not os.path.isdir('/tmp/uvfits'):
-		os.system('mkdir /tmp/uvfits')
-	directory_contents = os.listdir('/tmp/uvfits')
-
-	download_uvfits = True
-	download_metafits = True
-	for filename in directory_contents:
-		if filename == '{}.uvfits'.format(obs_id):
-			download_uvfits = False
-		if filename == '{}.metafits'.format(obs_id):
-			download_metafits = False
-	if download_uvfits:
-		print 'Downloading {}.uvfits from S3...'.format(obs_id)
-		sys.stdout.flush()
-		os.system('aws s3 cp s3://mwatest/uvfits/{}.{}/{}.uvfits /tmp/uvfits/{}.uvfits'.format(cotter_version, cotter_subversion, obs_id, obs_id))
-	if download_metafits:
-		print 'Downloading {}.metafits from S3...'.format(obs_id)
-		sys.stdout.flush()
-		os.system('aws s3 cp s3://mwatest/metafits/{}.{}/{}.metafits /tmp/uvfits/{}.metafits'.format(cotter_version, cotter_subversion, obs_id, obs_id))
-			
-			
-def run_firstpass(obs_id, version):
-
-	#Run firstpass
-	vis_file_list = '/tmp/uvfits/{}.uvfits'.format(obs_id)
-	output_directory = '/tmp'
-	if not os.path.isdir('/tmp/{}'.format(version)):
-		os.system('mkdir /tmp/{}'.format(version))
-	idl = pidly.IDL('/usr/local/bin/idl -IDL_DEVICE ps')
-	print 'Running firstpass on obsid {}...'.format(obs_id)
-	sys.stdout.flush()
-	idl('eor_firstpass_versions, /aws, obs_id = \'{}\', output_directory = \'{}\', version = \'{}\', vis_file_list = \'{}\''.format(obs_id, output_directory, version, vis_file_list))
-	idl.close()
-	
 	
 def error_procedure(log_filename):
+
 	os.system('aws s3 cp /tmp/{} s3://mwatest/FHD_FIRST_PASS/{}'.format(log_filename, log_filename))
 	sys.exit(1)
 	
